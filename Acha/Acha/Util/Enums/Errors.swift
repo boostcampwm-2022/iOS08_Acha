fdfa5baefa8cabf56280fb4135148fbdfa42b364
sangyeon <sangyeun33@gmail.com>
2022-11-15 20:44:35 +0900

    add: Errors Enum에 description 프로퍼티 추가

--- Acha/Acha/Util/Enums/Errors.swift
+++ Acha/Acha/Util/Enums/Errors.swift
@@ -10,4 +10,13 @@ import Foundation
 enum Errors {
     case decodeError
     case cannotDrawPolyLine
+    
+    var description: String {
+        switch self {
+        case .decodeError:
+            return "decoding에 실패했습니다."
+        case .cannotDrawPolyLine:
+            return "땅의 경계선(PolyLine)을 그릴 수 없습니다."
+        }
+    }
 }
