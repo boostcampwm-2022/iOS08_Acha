3f8170ea0ae77d70dcd91970aa61a1f5eb646714
godo129 <godo129@naver.com>
2022-11-15 17:57:58 +0900

    feat: Constants 추가 #32

---
+++ Acha/Acha/Util/Constants/FontConstants.swift
@@ -0,0 +1,13 @@
+//
+//  FontConstants.swift
+//  Acha
+//
+//  Created by hong on 2022/11/15.
+//
+
+import UIKit
+
+struct FontConstants {
+    static let gameModeBottomTabFont = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    static let titleFont = UIFont.systemFont(ofSize: 32, weight: .bold)
+}
