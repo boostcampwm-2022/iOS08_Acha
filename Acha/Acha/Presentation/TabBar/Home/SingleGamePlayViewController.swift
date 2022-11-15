8cb9628e5a373e58afc6617e2b6dbb380c7cdb0b
godo129 <godo129@naver.com>
2022-11-15 18:06:07 +0900

    feat: 혼자하기 게임 화면 UI 및 ViewModel 기본 코드

---
+++ Acha/Acha/Presentation/TabBar/Home/SingleGamePlayViewController.swift
@@ -0,0 +1,164 @@
+//
+//  SingleGamePlayViewController.swift
+//  Acha
+//
+//  Created by hong on 2022/11/15.
+//
+
+import UIKit
+import MapKit
+import RxSwift
+import RxCocoa
+
+final class SingleGamePlayViewController: UIViewController, DistanceAndTimeBarLine {
+    // MARK: - UI properties
+    private let mapView = MKMapView()
+    private let resetButton: UIButton = {
+        let button = UIButton()
+        button.setImage(
+            ImageConstants
+                .arrowPositionResetImage?
+                .withTintColor(
+                    ColorConstants.pointColor ?? UIColor(),
+                    renderingMode: .alwaysOriginal
+                ),
+            for: .normal
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    var distanceAndTimeBar = DistanceAndTimeBar()
+    private let rightMenuButton: UIButton = {
+        let button = UIButton()
+        button.setImage(
+            ImageConstants.inGameMenuButtonImage?
+                .withTintColor(
+                    ColorConstants.pointColor ?? UIColor(),
+                    renderingMode: .alwaysOriginal
+                ),
+            for: .normal
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    private let firstLoction = CLLocation(latitude: 37.517496, longitude: 126.959118)
+    private let distanceAndTimeView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    private let viewModel: SingleGamePlayViewModel
+    // MARK: - Lifecycles
+    
+    init(viewModel: SingleGamePlayViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        mapViewSetting()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        distanceAndTimeBarBottomLineAdjust(color: .white, width: 5)
+        distanceAndTimeBarMiddleLineAdjust(color: .white, width: 5)
+    }
+}
+
+// MARK: - Helpers
+extension SingleGamePlayViewController {
+    private func layout() {
+        addViews()
+        layoutViews()
+        bind()
+        rightMenuButtonSetting()
+    }
+    
+    private func addViews() {
+        view.addSubview(mapView)
+        view.addSubview(resetButton)
+        view.addSubview(distanceAndTimeBar)
+        view.addSubview(rightMenuButton)
+    }
+    
+    private func layoutViews() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        
+            rightMenuButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            rightMenuButton.heightAnchor.constraint(equalToConstant: 50),
+            rightMenuButton.widthAnchor.constraint(equalToConstant: 50),
+            rightMenuButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            
+            distanceAndTimeBar.widthAnchor.constraint(equalTo: view.widthAnchor),
+            distanceAndTimeBar.heightAnchor.constraint(equalToConstant: 100),
+            distanceAndTimeBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            resetButton.bottomAnchor.constraint(equalTo: distanceAndTimeBar.topAnchor),
+            resetButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            resetButton.widthAnchor.constraint(equalToConstant: 50),
+            resetButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func mapViewSetting() {
+        mapView.centerToLocation(firstLoction)
+    }
+    
+    private func rightMenuButtonSetting() {
+        let menuItems: [UIAction] =
+        [
+            UIAction(title: "랭킹", handler: { [weak self] _ in
+                let viewController = GamePlayMenuView(type: .ranking)
+                self?.presentModal(viewController: viewController)
+            }),
+            UIAction(title: "기록", handler: { [weak self] _ in
+                let viewController = GamePlayMenuView(type: .recordHistory)
+                self?.presentModal(viewController: viewController)
+            })
+        ]
+        let menu = UIMenu(title: "", children: menuItems)
+        rightMenuButton.menu = menu
+        rightMenuButton.showsMenuAsPrimaryAction = true
+    }
+    
+    private func presentModal(viewController: GamePlayMenuView) {
+        viewController.modalPresentationStyle = .pageSheet
+        present(viewController, animated: true)
+    }
+}
+
+extension SingleGamePlayViewController {
+    private func bind() {
+        resetButton.rx.tap.subscribe { [weak self] _ in
+            self?.mapViewSetting()
+        }
+        .disposed(by: disposeBag)
+    }
+}
+
+private extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 1000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius
+        )
+        setRegion(coordinateRegion, animated: true)
+    }
+}
