57fd756361efddbf6d4f4c6a79ac871ce678f40f
조승기 <beenzino@kookmin.ac.kr>
2022-11-16 12:32:20 +0900

    feat: extension Double -> degree - radian convert method

---
+++ Acha/Acha/Util/Extension/Double+.swift
@@ -0,0 +1,18 @@
+//
+//  Double+.swift
+//  Acha
+//
+//  Created by 조승기 on 2022/11/16.
+//
+
+import Foundation
+
+extension Double {
+    func deg2rad() -> Double {
+        self * .pi / 180
+     }
+    
+    func rad2deg() -> Double {
+        self * 180.0 / .pi
+    }
+}
