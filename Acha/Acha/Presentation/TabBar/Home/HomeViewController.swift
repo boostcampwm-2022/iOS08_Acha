feeb121da878aef7478e96e100f7c1c30559d327
조승기 <beenzino@kookmin.ac.kr>
2022-11-17 13:39:27 +0900

    chore: UIFont.폰트 -> .폰트

--- Acha/Acha/Presentation/TabBar/Home/HomeViewController.swift
+++ Acha/Acha/Presentation/TabBar/Home/HomeViewController.swift
@@ -31,7 +31,7 @@ final class HomeViewController: UIViewController {
         $0.layer.maskedCorners = cornerMask
         $0.layer.backgroundColor = (UIColor.pointLight ?? UIColor.red) .cgColor
         $0.numberOfLines = 0
-        $0.font = UIFont.boldSystemFont(ofSize: 34)
+        $0.font = .boldSystemFont(ofSize: 34)
         $0.textColor = .white
         $0.text = " 시작하기"
         $0.sizeToFit()
@@ -59,7 +59,7 @@ final class HomeViewController: UIViewController {
     private lazy var startSingleGameButton = UIButton().then {
         $0.layer.backgroundColor = UIColor(named: "PointDarkColor")?.cgColor
         $0.setTitle("혼자 하기", for: .normal)
-        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.titleLabel?.font = .boldSystemFont(ofSize: 30)
         $0.tintColor = .white
         $0.layer.cornerRadius = 10
     }
@@ -67,7 +67,7 @@ final class HomeViewController: UIViewController {
     private lazy var startMultiGameButton = UIButton().then {
         $0.layer.backgroundColor = UIColor(named: "PointDarkColor")?.cgColor
         $0.setTitle("같이 하기", for: .normal)
-        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
+        $0.titleLabel?.font = .boldSystemFont(ofSize: 30)
         $0.tintColor = .white
         $0.layer.cornerRadius = 10
     }
