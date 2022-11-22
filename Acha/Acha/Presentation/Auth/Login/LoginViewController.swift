2cbe3b4f54cf4519f883d6cbbdbdfc40b06be8f2
godo129 <godo129@naver.com>
2022-11-22 11:20:18 +0900

    feat: 로그인 뷰 뷰모델 뷰 컨트롤러 파일 추가

--- Acha/Acha/Presentation/Auth/Login/LoginViewController.swift
+++ Acha/Acha/Presentation/Auth/Login/LoginViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
-class LoginViewController: UIViewController {
-
+final class LoginViewController: UIViewController {
+    
+    private let emailTextField = AuthInputTextField(type: .email)
+    private let passwordTextField = AuthInputTextField(type: .password)
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
-        // Do any additional setup after loading the view.
+        
+        layout()
     }
+}
+
+extension LoginViewController {
+    
+    private func layout() {
+        
+    }
+    
+    private func addViews() {
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+    }
+    
+    private func addConstraints() {
+        
+    }
+    
 }
