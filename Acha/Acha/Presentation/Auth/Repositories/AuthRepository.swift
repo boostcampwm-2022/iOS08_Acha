7064980c80cbab9ad7d90e38d7deb48042ae5145
godo129 <godo129@naver.com>
2022-11-22 11:22:20 +0900

    feat: 회원 가입 레포지토리

---
+++ Acha/Acha/Presentation/Auth/Repositories/AuthRepository.swift
@@ -0,0 +1,47 @@
+//
+//  AuthRepository.swift
+//  Acha
+//
+//  Created by hong on 2022/11/21.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+protocol SignUpRepository {
+    var emailRelay: BehaviorRelay<String> {get set}
+    var passwordRelay: BehaviorRelay<String> {get set}
+    var nickNameRelay: BehaviorRelay<String> {get set}
+    
+    var emailValidation: Bool {get set}
+    var passwordValidation: Bool {get set}
+    var nickNameValidation: Bool {get set}
+    
+    func getSignUpdata() -> Observable<SignUpData>
+    func isSignAble() -> Bool
+}
+
+final class AuthRepository: SignUpRepository {
+    var emailValidation: Bool = false
+    var passwordValidation: Bool = false
+    var nickNameValidation: Bool = false
+    
+    var emailRelay: RxRelay.BehaviorRelay<String> = .init(value: "")
+    
+    var passwordRelay: RxRelay.BehaviorRelay<String> = .init(value: "")
+    
+    var nickNameRelay: RxRelay.BehaviorRelay<String> = .init(value: "")
+    
+    func getSignUpdata() -> RxSwift.Observable<SignUpData> {
+        return Observable
+            .combineLatest(emailRelay, passwordRelay, nickNameRelay)
+            .map { (email, password, nickName) in
+                return SignUpData(email: email, password: password, nickName: nickName)
+            }
+    }
+    
+    func isSignAble() -> Bool {
+        return emailValidation == true && passwordValidation == true && nickNameValidation == true 
+    }
+}
