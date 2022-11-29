8dd1a71869ee27815529e5ad8f796b0b7f241983
godo129 <godo129@naver.com>
2022-11-29 21:55:46 +0900

    feat: 셀 레이아웃

--- Acha/Acha/Presentation/TabBar/Community/Cell/CommentCollectionViewCell.swift
+++ Acha/Acha/Presentation/TabBar/Community/Cell/CommentCollectionViewCell.swift
@@ -26,4 +26,47 @@ final class CommentCollectionViewCell: UICollectionViewCell {
         $0.font = .body
     }
     
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension CommentCollectionViewCell {
+    private func layout() {
+        addViews()
+        addConstraints()
+    }
+    
+    private func addViews() {
+        addSubview(nickNameLabel)
+        addSubview(timeLabel)
+        addSubview(contextLabel)
+    }
+    
+    private func addConstraints() {
+        nickNameLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().inset(7)
+            $0.width.equalTo(100)
+            $0.height.equalTo(50)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.top.equalTo(nickNameLabel)
+            $0.leading.equalTo(nickNameLabel.snp.trailing).inset(20)
+            $0.height.equalTo(50)
+            $0.width.equalTo(100)
+        }
+        
+        contextLabel.snp.makeConstraints {
+            $0.top.equalTo(nickNameLabel).inset(30)
+            $0.leading.equalTo(nickNameLabel)
+            $0.trailing.bottom.equalToSuperview()
+        }
+    }
 }
