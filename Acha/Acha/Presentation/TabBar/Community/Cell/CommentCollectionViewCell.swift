a0a78baa11e9db12ff6324eca19f496e23f56490
godo129 <godo129@naver.com>
2022-11-29 21:36:17 +0900

    feat: commentCollectionViewCell

--- Acha/Acha/Presentation/TabBar/Community/Cell/CommentCollectionViewCell.swift
+++ Acha/Acha/Presentation/TabBar/Community/Cell/CommentCollectionViewCell.swift
@@ -10,5 +10,20 @@ import UIKit
 final class CommentCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "CommentCollectionViewCell"
+
+    private lazy var nickNameLabel = UILabel().then {
+        $0.font = .boldBody
+        $0.textAlignment = .center
+        $0.textColor = .black
+    }
+    
+    private lazy var timeLabel = UILabel().then {
+        $0.font = .smallTitle
+        $0.textColor = .black
+    }
+    
+    private lazy var contextLabel = UILabel().then {
+        $0.font = .body
+    }
     
 }
--- Acha/Acha/Presentation/TabBar/Community/Cell/PostCollectionViewCell.swift
+++ Acha/Acha/Presentation/TabBar/Community/Cell/PostCollectionViewCell.swift
@@ -6,9 +6,42 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class PostCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "PostCollectionViewCell"
     
+    private lazy var contextLabel = UILabel().then {
+        $0.font = .body
+        $0.textColor = .black
+        $0.numberOfLines = 0
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PostCollectionViewCell {
+    private func layout() {
+        addViews()
+        addConstraints()
+    }
+    
+    private func addViews() {
+        addSubview(contextLabel)
+    }
+    
+    private func addConstraints() {
+        contextLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
