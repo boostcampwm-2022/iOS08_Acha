db1ec96b99ac81ac4fd8747ecf8f9616d8a1edd8
sangyeon <sangyeun33@gmail.com>
2022-12-02 00:53:58 +0900

    add: User Entity 구현

---
+++ Acha/Acha/Domain/Entity/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Acha
+//
+//  Created by  sangyeon on 2022/12/01.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let nickName: String
+    let badges: [Int]
+    let records: [Int]
+//    let pinCharacter: String
+    let friends: [Int]
+}
