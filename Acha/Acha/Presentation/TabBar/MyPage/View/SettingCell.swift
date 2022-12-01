c585e291c542dbd1af6b1bcb7e4e3e9719b0f65f
sangyeon <sangyeun33@gmail.com>
2022-12-02 00:55:45 +0900

    chore: SettingCell 수정

--- Acha/Acha/Presentation/TabBar/MyPage/View/SettingCell.swift
+++ Acha/Acha/Presentation/TabBar/MyPage/View/SettingCell.swift
@@ -9,36 +9,51 @@ import UIKit
 import Then
 import SnapKit
 
-class SettingCell: UICollectionViewCell {
+final class SettingCell: UICollectionViewCell {
     // MARK: - UI properties
-    private lazy var titleLabel: UILabel = UILabel().then {
-        $0.font = .commentBody
-        $0.textAlignment = .left
+    private lazy var button: UIButton = UIButton().then {
+        $0.titleLabel?.font = .commentBody
+        $0.setTitleColor(.black, for: .normal)
+        $0.contentHorizontalAlignment = .left
     }
 
     // MARK: - Properties
     static let identifer = "SettingCell"
+    var buttonHandler: (() -> Void)?
     
     // MARK: - Lifecycles
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupSubviews()
+        setupButton()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     // MARK: - Helpers
     private func setupSubviews() {
-        addSubview(titleLabel)
+        addSubview(button)
         
-        titleLabel.snp.makeConstraints {
+        button.snp.makeConstraints {
             $0.top.bottom.equalToSuperview()
             $0.leading.trailing.equalToSuperview().inset(10)
         }
     }
     
-    func bind(title: String) {
-        titleLabel.text = title
+    private func setupButton() {
+        button.addTarget(self,
+                         action: #selector(buttonDidClick),
+                         for: .touchUpInside)
+    }
+    
+    @objc func buttonDidClick() {
+        buttonHandler?()
+    }
+    
+    func bind(title: String, handler: (() -> Void)?) {
+        button.setTitle(title, for: .normal)
+        self.buttonHandler = handler
     }
 }
