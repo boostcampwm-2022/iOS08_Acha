b4a7dae40a88b0ccbb780cdfeeaeeade2ebcc3f1
sangyeon <sangyeun33@gmail.com>
2022-12-02 00:54:23 +0900

    feat: MyPage UseCase 구현

---
+++ Acha/Acha/Domain/UseCase/Protocol/MyPageUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  MyPageUseCase.swift
+//  Acha
+//
+//  Created by  sangyeon on 2022/12/01.
+//
+
+import RxSwift
+
+protocol MyPageUseCase {
+    var userInfo: User? { get set }
+    var nickName: PublishSubject<String> { get set }
+    var ownedBadges: BehaviorSubject<[Badge]> { get set }
+    var allBadges: BehaviorSubject<[Badge]> { get set }
+    func fetchMyPageData()
+}
