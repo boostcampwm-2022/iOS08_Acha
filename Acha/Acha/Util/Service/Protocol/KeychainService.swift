5867904f19a00590b505c076ed85270a9c1e2a8c
godo129 <godo129@naver.com>
2022-12-03 17:26:54 +0900

    feat: keychain service

---
+++ Acha/Acha/Util/Service/Protocol/KeychainService.swift
@@ -0,0 +1,22 @@
+//
+//  KeychainService.swift
+//  Acha
+//
+//  Created by hong on 2022/12/02.
+//
+import Foundation
+
+protocol KeychainService {
+
+    /// uuid 받아 오는 메서드
+    func get() -> String?
+
+    /// uuid 삭제 메서드
+    func delete()
+
+    /// uuid 저장 메서드
+    func save(uuid: String)
+
+    /// uuid 업데이트 메서드
+    func update(uuid: String)
+}
