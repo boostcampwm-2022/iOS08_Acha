1bf45c0c0af0f8d2515f473fbed42adefb0267c4
godo129 <godo129@naver.com>
2022-12-06 13:45:36 +0900

    feat: Time Repository 생성

---
+++ Acha/Acha/Data/Repository/DefaultTimeRepository.swift
@@ -0,0 +1,34 @@
+//
+//  DefaultTimeRepository.swift
+//  Acha
+//
+//  Created by hong on 2022/12/06.
+//
+
+import Foundation
+import RxSwift
+
+struct DefaultTimeRepository {
+    
+    private let timerService: TimerServiceProtocol
+    
+    init(timeService: TimerServiceProtocol) {
+        self.timerService = timeService
+    }
+    
+    func startTimer() -> Observable<Int> {
+        return timerService.start()
+    }
+    
+    func setTimer(time: Int) -> Observable<Int> {
+        return startTimer()
+            .map {
+                let remainTime = time - $0
+                if remainTime <= 0 { stopTimer() }
+            }
+    }
+    
+    func stopTimer() {
+        return timerService.stop()
+    }
+}
