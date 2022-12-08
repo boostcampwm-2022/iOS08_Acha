f0b9fbe6f676c5529e4803c735e479a4445da15e
godo129 <godo129@naver.com>
2022-12-09 05:26:19 +0900

    feat: Rx 상태 관련 코드

---
+++ Acha/Acha/Util/Extension/UIApplication+Rx.swift
@@ -0,0 +1,68 @@
+//
+//  UIApplication+Rx.swift
+//  Acha
+//
+//  Created by hong on 2022/12/09.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+public enum Appstate: Equatable {
+    case active
+    case inactive
+    case background
+    case terminated
+}
+
+extension Reactive where Base: UIApplication {
+    var applicationWillEnterForeground: Observable<Appstate> {
+        return NotificationCenter.default.rx.notification(UIApplication.willEnterForegroundNotification)
+            .map { _ in
+                return .active
+            }
+    }
+    
+    var applicationDidBecomActive: Observable<Appstate> {
+        return NotificationCenter.default.rx.notification(UIApplication.didBecomeActiveNotification)
+            .map { _ in
+                return .active
+            }
+    }
+    
+    var applicationDidEnterBackground: Observable<Appstate> {
+        return NotificationCenter.default.rx.notification(UIApplication.didEnterBackgroundNotification)
+            .map { _ in
+                return .background
+            }
+    }
+    
+    var applicationWillResignActive: Observable<Appstate> {
+        return NotificationCenter.default.rx.notification(UIApplication.willResignActiveNotification)
+            .map { _ in
+                return .inactive
+            }
+    }
+    
+    var applicationWillTerminate: Observable<Appstate> {
+        return NotificationCenter.default.rx.notification(UIApplication.willTerminateNotification)
+            .map { _ in
+                return .terminated
+            }
+    }
+    
+    public var appState: Observable<Appstate> {
+        return Observable.of(
+            applicationDidBecomActive,
+            applicationWillResignActive,
+            applicationWillEnterForeground,
+            applicationDidEnterBackground,
+            applicationWillTerminate
+        )
+        .merge()
+    }
+    
+}
+
+
