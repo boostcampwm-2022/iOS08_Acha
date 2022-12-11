d2e5b1fe21219297f3fc0ee1692ce835856f847c
조승기 <beenzino@kookmin.ac.kr>
2022-12-11 19:45:21 +0900

    feat: 훔쳐온 파일 작성

--- Acha/Acha/Util/Service/Protocol/FirebaseStorageNetworkService.swift
+++ Acha/Acha/Util/Service/Protocol/FirebaseStorageNetworkService.swift
@@ -2,7 +2,13 @@
 //  FirebaseStorageNetworkService.swift
 //  Acha
 //
-//  Created by 조승기 on 2022/12/10.
+//  Created by 배남석 on 2022/12/07.
 //
 
 import Foundation
+import RxSwift
+
+protocol FirebaseStorageNetworkService {
+    func upload(type: FirebaseStorageType, data: Data, completion: @escaping (URL?) -> Void)
+    func download(urlString: String, completion: @escaping (Data?) -> Void)
+}
