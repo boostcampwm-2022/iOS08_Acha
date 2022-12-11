303ee6361eddf7c15b8d0f6d77d630f0f1307eb8
조승기 <beenzino@kookmin.ac.kr>
2022-12-11 19:48:02 +0900

    refactor: BadgeDTO toDomain 시점에 이미지 다운로드 ( 캐시 ) 하도록 수정

--- Acha/Acha/Data/Repository/DefaultBadgeRepository.swift
+++ Acha/Acha/Data/Repository/DefaultBadgeRepository.swift
@@ -6,22 +6,43 @@
 //
 
 import RxSwift
+import Foundation
 
 final class DefaultBadgeRepository: BadgeRepository {
     private let realTimeDatabaseNetworkService: RealtimeDatabaseNetworkService
+    private let firebaseStorageNetworkService: FirebaseStorageNetworkService
+    private let imageCacheService: ImageCacheService
+    private var disposeBag = DisposeBag()
     
-    init(realTimeDatabaseNetworkService: RealtimeDatabaseNetworkService) {
+    init(
+        realTimeDatabaseNetworkService: RealtimeDatabaseNetworkService,
+        firebaseStorageNetworkService: FirebaseStorageNetworkService,
+        imageCacheService: ImageCacheService
+    ) {
         self.realTimeDatabaseNetworkService = realTimeDatabaseNetworkService
+        self.firebaseStorageNetworkService = firebaseStorageNetworkService
+        self.imageCacheService = imageCacheService
     }
     
-    func fetchAllBadges() -> Single<[Badge]> {
+    func fetchAllBadges() -> Observable<[Badge]> {
         realTimeDatabaseNetworkService.fetch(type: FirebaseRealtimeType.badge)
-            .map { (badgeDTOs: [BadgeDTO]) in
-                badgeDTOs.map { $0.toDomain() }
+            .asObservable()
+            .flatMap { (badgeDTOs: [BadgeDTO]) in
+                Observable.zip(badgeDTOs.map { badgeDTO in
+                    if let data = self.imageCacheService.load(imageURL: badgeDTO.imageURL) {
+                        return Observable.of(badgeDTO.toDomain(image: data))
+                    }
+                    
+                    return self.firebaseStorageNetworkService.download(urlString: badgeDTO.imageURL)
+                        .map { data -> Badge in
+                            self.imageCacheService.write(imageURL: badgeDTO.imageURL, image: data)
+                            return badgeDTO.toDomain(image: data)
+                        }
+                })
             }
     }
     
-    func fetchSomeBadges(ids: [Int]) -> Single<[Badge]> {
+    func fetchSomeBadges(ids: [Int]) -> Observable<[Badge]> {
         fetchAllBadges()
             .map { (badges: [Badge]) -> [Badge] in
                 return badges.filter { ids.contains($0.id) }
